d9e3dc75ace687a5a43f83910b4f04a69940f749
Semkoo <semirnik2@gmail.com>
2017-11-30 20:24:22 -0500

    added location page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,7 +1,12 @@
 /* GET 'home' page. */
+// module.exports.homelist = function(req,res){
+//     res.render('index', {title:'Home'});
+// };
 module.exports.homelist = function(req,res){
-    res.render('index', {title:'Home'});
+    res.render('locations-list', {title:'Home'});
 };
+
+
 /* GET 'Location Info' page */
 module.exports.locationInfo = function(req,res){
     res.render('index', {title:'Location Info'});
@@ -11,3 +16,4 @@ module.exports.locationInfo = function(req,res){
 module.exports.addReview = function(req,res){
     res.render('index', {title:'Add Review'});
 };
+

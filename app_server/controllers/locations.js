16712a421e9c39da9d5c1104318493d37c49bef3
Semkoo <semirnik2@gmail.com>
2018-02-24 22:01:03 -0500

    Added locations list infp

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,7 +3,36 @@
 //     res.render('index', {title:'Home'});
 // };
 module.exports.homelist = function(req,res){
-    res.render('locations-list', {title:'Home'});
+    res.render('locations-list', {
+        title:'WifiSpoof',
+        pageHeader : {
+            title:'WifiSpoof',
+            strapline: 'Find place to work with wifi near you!'
+        },
+        locations:[
+            {
+                name:'Starcups',
+                address:'125 High Street, Reading, RG6 1PS',
+                rating: 3, 
+                facilities: ['Hot Drinks', 'Food', 'Premium Wifi'],
+                distance: '100m'
+            },
+            {
+                name:'Cafe Hero',
+                address:'125 High Street, Reading, RG6 1PS',
+                rating: 4, 
+                facilities: ['Hot Drinks', 'Food', 'Premium Wifi'],
+                distance: '200m'
+            },
+            {
+                name:'Burger Queen',
+                address:'125 High Street, Reading, RG6 1PS',
+                rating: 2, 
+                facilities: [ 'Food', 'Premium Wifi'],
+                distance: '250'
+            },
+        ]
+    });
 };
 
 
